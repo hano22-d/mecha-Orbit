1d578aa8e3f2382b0fae8335d4ee71a571b291ab
hani joma <jomahani41@gmail.com>
2026-07-11 01:31:48 +0300

    a

--- src/systems/InputHandler.js
+++ src/systems/InputHandler.js
@@ -154,7 +154,7 @@ export class InputsHandle {
 
     if (!joystickTouched && base && knob) {
       knob.x = base.startX;
-      knob.y = base.startY - 20;
+      knob.y = base.startY - 10;
     }
 
     // شبكة الأمان الكلية عند خلو الشاشة تماماً
