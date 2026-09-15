aea218c3c12e114d412a8b07b84761d7815a8535
hano22-d <jomahani41@gmail.com>
2026-09-15 12:12:20 +0300

    Refactor Chaser class by cleaning comments
    Removed commented Arabic text and added update method header.

--- src/entities/enemies/Chaser.js
+++ src/entities/enemies/Chaser.js
@@ -4,7 +4,6 @@ import { Enemy } from "./Enemy";
 
 export class Chaser extends Enemy {
   constructor(config) {
-    // config يحتوي بالفعل على الـ canvas والـ x والـ y الممررين من محرك اللعبة الرئيسي
     super({
       ...config,
       baseWidth: 125,
@@ -22,6 +21,9 @@ export class Chaser extends Enemy {
     });
   }
 
+  /* ============
+      دالة التحديث
+     ============ */
   update(time, deltaTime, game, camera) {
     let dx = game.player.x - this.x;
     let dy = game.player.y - this.y;
