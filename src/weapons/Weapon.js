e33054f1c90d0e1f61ab9e206a627155e458c465
hano22-d <jomahani41@gmail.com>
2026-09-16 13:56:06 +0300

    Update comments in Weapon class

--- src/weapons/Weapon.js
+++ src/weapons/Weapon.js
@@ -1,10 +1,9 @@
-//normal bullet class
 export class Weapon {
     constructor(owner) {
       this.owner = owner;
     }
 
     shoot(bullets) {
-      // سيتم override لاحقاً
+      // سيتم تعبئة الدالة في كلاسات الأبناء
     }
   }
